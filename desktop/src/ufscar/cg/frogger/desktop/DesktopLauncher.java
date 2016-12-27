4718c767f6b6e9839c533f6691bcff9d1442fd6d
Brandao <joao.brandao@vistajet.com>
2016-12-27 20:49:30 -0200

    Initial commit

---
+++ desktop/src/ufscar/cg/frogger/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ufscar.cg.frogger.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ufscar.cg.frogger.Frogger;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Frogger(), config);
+	}
+}
