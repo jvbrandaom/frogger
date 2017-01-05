89652d2cd935c241bb0f60c03fd5cd0310c9e289
Rafael Silveira <rafesilveira@hotmail.com>
2017-01-04 23:50:03 -0200

    Set window icon

--- desktop/src/ufscar/cg/frogger/desktop/DesktopLauncher.java
+++ desktop/src/ufscar/cg/frogger/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package ufscar.cg.frogger.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ufscar.cg.frogger.Frogger;
@@ -7,6 +8,7 @@ import ufscar.cg.frogger.Frogger;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.addIcon("core/assets/icon.png", Files.FileType.Internal);
 		new LwjglApplication(new Frogger(), config);
 	}
 }
