f34e9b1a024904d895ba67c5c80eed0c50a658f8
Rafael Silveira <rafesilveira@hotmail.com>
2017-01-05 20:44:45 -0200

    Add more frogs to menu screen

--- core/src/ufscar/cg/frogger/screens/MenuScreen.java
+++ core/src/ufscar/cg/frogger/screens/MenuScreen.java
@@ -10,13 +10,16 @@ import com.badlogic.gdx.utils.Array;
 import ufscar.cg.frogger.Frogger;
 import ufscar.cg.frogger.data.ImageCache;
 
-
 public class MenuScreen extends Screen {
 
     private BitmapFont title;
     private Animation<Sprite> animation;
     private float elapsedTime = 0f;
-    int y = 0;          // y used as an iterator to set the vertical position of the frog
+    Array<Sprite> sprites;
+    Array<Integer> frogsX;
+    Array<Integer> frogsY;
+    Sprite keyFrame;
+    float y = 0;          // y used to set the vertical position of the frog
 
     public MenuScreen (Frogger game) {
         super(game);
@@ -26,14 +29,23 @@ public class MenuScreen extends Screen {
     public void createScreen() {
         title = new BitmapFont();
 
+        initializeFrogsPos();
+
         // get sprites for frog standing and jumping
         if (elements.size() == 0) {
-            Array<Sprite> sprites = new Array<Sprite>();
+            sprites = new Array<Sprite>();
             Sprite sprite1 = new Sprite(ImageCache.getTexture("frog"));
             Sprite sprite2 = new Sprite(ImageCache.getTexture("frog_jump"));
             sprites.add(sprite1);
             sprites.add(sprite2);
-            animation = new Animation<Sprite>(0.5f, sprites);
+            animation = new Animation<Sprite>(0.3f, sprites);
+        }
+
+        for (int i = 0; i < frogsX.size; i++){
+            keyFrame = animation.getKeyFrame(elapsedTime, true);
+            keyFrame.setX(frogsX.get(i));
+            keyFrame.setY(frogsY.get(i));
+            elements.add(keyFrame);
         }
     }
 
@@ -53,17 +65,41 @@ public class MenuScreen extends Screen {
             game.camera.update();
             game.batch.begin();
             title.setColor(Color.GREEN);
-            title.draw(game.batch, "Frogger\n\nPress Enter or Click to Play", 240, 280);
+            title.draw(game.batch, "               Frogger\n\nPress ENTER or Click to play", 220, 280);
 
-            // animate frog
-            y++;
+            // move and animate frogs
+            y+=1.5f;
             elapsedTime += dt;
-            Sprite keyFrame = animation.getKeyFrame(elapsedTime, true);
-            keyFrame.setY(y);
-            keyFrame.setX(240);
-            keyFrame.draw(game.batch);
+
+            for(int i = 0; i < frogsX.size; i++){
+                keyFrame = animation.getKeyFrame(elapsedTime, true);
+                keyFrame.setX(frogsX.get(i));
+                keyFrame.setY((frogsY.get(i)+y)%game.screenHeight);
+                keyFrame.draw(game.batch);
+            }
             game.batch.end();
         }
     }
-}
 
+    private void initializeFrogsPos(){
+        frogsX = new Array<Integer>();
+        frogsY = new Array<Integer>();
+
+        frogsX.add(50);
+        frogsY.add(0);
+        frogsX.add(100);
+        frogsY.add(20);
+        frogsX.add(220);
+        frogsY.add(30);
+        frogsX.add(300);
+        frogsY.add(60);
+        frogsX.add(350);
+        frogsY.add(40);
+        frogsX.add(400);
+        frogsY.add(30);
+        frogsX.add(450);
+        frogsY.add(40);
+        frogsX.add(500);
+        frogsY.add(20);
+    }
+}
